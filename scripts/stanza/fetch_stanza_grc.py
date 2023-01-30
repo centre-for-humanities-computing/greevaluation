58ff068d657369e86c86f12f514bf7016dd3ea4b
jankounchained <honza.anfas@gmail.com>
2023-01-30 16:59:04 +0100

    donwload & evaluate both stanza models
    fix delete

--- scripts/stanza/fetch_stanza_grc.py
+++ scripts/stanza/fetch_stanza_grc.py
@@ -6,6 +6,14 @@ Save to disk for usage in spacy CLI.
 import stanza
 import spacy_stanza
 
-stanza.download("grc")
-nlp = spacy_stanza.load_pipeline("grc")
-nlp.to_disk('models/stanza-model')
+# donwload newest stanza models
+stanza.download(lang='grc', package='perseus')
+stanza.download(lang='grc', package='proiel')
+
+# load as spacy models
+model_perseus = spacy_stanza.load_pipeline('grc', package='perseus')
+model_proiel = spacy_stanza.load_pipeline('grc', package='proiel')
+
+# serialize
+model_perseus.to_disk('models/stanza-perseus')
+model_proiel.to_disk('models/stanza-proiel')
