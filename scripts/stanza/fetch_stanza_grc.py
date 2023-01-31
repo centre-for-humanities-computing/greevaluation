ae8e5d3c8948c824e8eea8c9f5411c8cf2762ee9
jankounchained <honza.anfas@gmail.com>
2023-01-31 12:42:15 +0100

    fix: paths, dependencies, prevent double downloading of stanza models

--- scripts/stanza/fetch_stanza_grc.py
+++ scripts/stanza/fetch_stanza_grc.py
@@ -3,17 +3,19 @@ Download stanza grc pipeline.
 Save to disk for usage in spacy CLI.
 '''
 
+import os
 import stanza
 import spacy_stanza
 
-# donwload newest stanza models
-stanza.download(lang='grc', package='perseus')
-stanza.download(lang='grc', package='proiel')
+if not os.path.exists('models/stanza-perseus') and not os.path.exists("models/stanza-proiel"):
+    # donwload newest stanza models
+    stanza.download(lang='grc', package='perseus')
+    stanza.download(lang='grc', package='proiel')
 
-# load as spacy models
-model_perseus = spacy_stanza.load_pipeline('grc', package='perseus')
-model_proiel = spacy_stanza.load_pipeline('grc', package='proiel')
+    # load as spacy models
+    model_perseus = spacy_stanza.load_pipeline('grc', package='perseus')
+    model_proiel = spacy_stanza.load_pipeline('grc', package='proiel')
 
-# serialize
-model_perseus.to_disk('models/stanza-perseus')
-model_proiel.to_disk('models/stanza-proiel')
+    # serialize
+    model_perseus.to_disk('models/stanza-perseus')
+    model_proiel.to_disk('models/stanza-proiel')
