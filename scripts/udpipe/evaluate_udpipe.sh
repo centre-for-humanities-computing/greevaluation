fa68e92be9dc06719987bd6182ee345e1ac4a529
jankounchained <honza.anfas@gmail.com>
2023-02-01 18:37:16 +0100

    fix: paths

--- scripts/udpipe/evaluate_udpipe.sh
+++ scripts/udpipe/evaluate_udpipe.sh
@@ -1,5 +1,5 @@
 source environments/udpipe/bin/activate
-python3 scripts/fetch_udpipe_grc.py
-python3 scripts/predict_udpipe.py
-python3 scripts/evaluate_udpipe.py
+python3 scripts/udpipe/fetch_udpipe_grc.py
+python3 scripts/udpipe/predict_udpipe.py
+python3 scripts/udpipe/evaluate_udpipe.py
 deactivate
