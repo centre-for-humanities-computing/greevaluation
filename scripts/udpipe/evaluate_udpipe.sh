2a76936d5d649e2d86bd52669f0c38de4e58f574
jankounchained <honza.anfas@gmail.com>
2023-02-01 18:31:50 +0100

    udpipe predictions, bug: doesn't save anything
    fix: it runs now

--- scripts/udpipe/evaluate_udpipe.sh
+++ scripts/udpipe/evaluate_udpipe.sh
@@ -1,22 +1,5 @@
-# HACK: hardcoded following https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3131
-VERSION="2.5.0" 
-
-# get in stanza venv
 source environments/udpipe/bin/activate
-
-# fetch stanza models
-python3 scripts/udpipe/fetch_udpipe_grc.py
-
-# evaluate each model
-for UDPIPE_MODEL in "udpipe-perseus" "udpipe-proiel"
-do
-    # evaluate test fold in each corpus
-    for CORPUS in "joint" "perseus" "proiel"
-    do
-        mkdir -p "metrics/$UDPIPE_MODEL/$VERSION"
-        python3 -m spacy benchmark accuracy \
-            "models/$UDPIPE_MODEL" \
-            "corpus/binary/$CORPUS.spacy" \
-            --output "metrics/$UDPIPE_MODEL/$VERSION/$CORPUS.json"
-    done
-done
+python3 scripts/fetch_udpipe_grc.py
+python3 scripts/predict_udpipe.py
+python3 scripts/evaluate_udpipe.py
+deactivate
