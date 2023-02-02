dbb7a570b69486bef9e06cbfe4109ad8072325f2
Márton Kardos <power.up1163@gmail.com>
2023-02-02 16:50:23 +0100

    Added evaluation scripts for homercy

--- scripts/create_environments.sh
+++ scripts/create_environments.sh
@@ -25,3 +25,12 @@ source environments/udpipe/bin/activate
 pip install -r "requirements_udpipe.txt"
 pip install https://huggingface.co/janko/grc_dep_treebanks_trf/resolve/main/grc_dep_treebanks_trf-any-py3-none-any.whl
 deactivate
+
+# our stuff
+echo "Installing VENV: homercy"
+python3 -m venv environments/homercy
+source environments/homercy/bin/activate
+pip install -r "requirements_homercy.txt"
+pip install https://huggingface.co/janko/grc_dep_treebanks_trf/resolve/main/grc_dep_treebanks_trf-any-py3-none-any.whl
+pip install https://huggingface.co/janko/grc_dep_treebanks_sm/resolve/main/grc_dep_treebanks_sm-any-py3-none-any.whl
+deactivate
---
+++ scripts/homercy/evaluate_homercy.sh
@@ -0,0 +1,14 @@
+source environments/homercy/bin/activate
+
+VERSION="0.5.0"
+for MODEL in "grc_dep_treebanks_trf" "grc_dep_treebanks_sm" 
+do
+    mkdir -p metrics/$MODEL/$VERSION/
+    for CORPUS in "perseus" "proiel" "joint" 
+    do
+        echo "Evaluating $MODEL on $CORPUS"
+        python3 -m spacy benchmark accuracy $MODEL corpus/binary/$CORPUS.spacy --output metrics/$MODEL/$VERSION/$CORPUS.json
+    done
+done
+
+deactivate
