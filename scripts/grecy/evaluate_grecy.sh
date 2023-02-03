8e62749acbd64388ea61dc31dcd18277429c4da7
jankounchained <42962106+jankounchained@users.noreply.github.com>
2023-02-03 12:38:01 +0100

    GreCy eval (#16)
    * grecy evaluation script from homercy
    
    * eval results for grecy

---
+++ scripts/grecy/evaluate_grecy.sh
@@ -0,0 +1,47 @@
+###
+### grecy proiel
+###
+
+MODEL1="grc_ud_proiel_trf"
+
+# create a new venv
+python3 -m venv environments/$MODEL1
+source environments/$MODEL1/bin/activate
+echo "Using $VIRTUAL_ENV"
+
+# fetch model
+pip uninstall $MODEL1 -y
+pip install "https://huggingface.co/Jacobo/$MODEL1/resolve/main/$MODEL1-any-py3-none-any.whl"
+
+# eval proiel
+for CORPUS in "joint" "perseus" "proiel"
+    do
+    mkdir -p "metrics/$MODEL1/1.0.0/"
+    spacy evaluate "$MODEL1" corpus/binary/$CORPUS.spacy --output "metrics/$MODEL1/1.0.0/$CORPUS.json"
+done
+
+deactivate
+
+###
+### grecy perseus
+###
+
+MODEL2="grc_ud_perseus_trf"
+
+# create a new venv
+python3 -m venv environments/$MODEL2
+source environments/$MODEL2/bin/activate
+echo "Using $VIRTUAL_ENV"
+
+# fetch model
+pip uninstall $MODEL2 -y
+pip install "https://huggingface.co/Jacobo/$MODEL2/resolve/main/$MODEL2-any-py3-none-any.whl"
+
+# eval perseus
+for CORPUS in "joint" "perseus" "proiel"
+    do
+    mkdir -p "metrics/$MODEL2/1.0.0/"
+    spacy evaluate "$MODEL2" corpus/binary/$CORPUS.spacy --output "metrics/$MODEL2/1.0.0/$CORPUS.json"
+done
+
+deactivate
