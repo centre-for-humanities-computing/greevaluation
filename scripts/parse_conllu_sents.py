d7c575482361cc7c8f394e1aa386226bdb9d1e97
Márton Kardos <power.up1163@gmail.com>
2023-02-06 11:42:49 +0100

    Created Spark prediction script

--- scripts/parse_conllu_sents.py
+++ scripts/parse_conllu_sents.py
@@ -4,16 +4,15 @@ import os
 from conllu import parse
 
 
-CONLLU_DIR = 'corpus/conllu'
-OUT_DIR = 'corpus/text_sents'
+CONLLU_DIR = "corpus/conllu"
+OUT_DIR = "corpus/text_sents"
 
 
 if not os.path.exists(OUT_DIR):
     os.mkdir(OUT_DIR)
 
 
-for corpus in ['joint', 'perseus', 'proiel']:
-
+for corpus in ["joint", "perseus", "proiel"]:
     # load conllu
     path = os.path.join(CONLLU_DIR, f"{corpus}.conllu")
     with open(path) as fin:
@@ -21,10 +20,10 @@ for corpus in ['joint', 'perseus', 'proiel']:
 
     # parse & extract segmented sentences
     treebank = parse(raw_data)
-    sentences = [sent.metadata['text'] for sent in treebank]
+    sentences = [sent.metadata["text"] for sent in treebank]
 
     # export to txt
     outpath = os.path.join(OUT_DIR, f"{corpus}.txt")
-    with open(outpath, 'w') as fin:
+    with open(outpath, "w") as fin:
         for sent in sentences:
             fin.write(f"{sent}\n")
