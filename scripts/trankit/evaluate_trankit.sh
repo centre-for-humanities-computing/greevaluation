a2c4258777e248382ead32347fde4b6e3ac478d6
Márton Kardos <power.up1163@gmail.com>
2023-02-07 11:11:29 +0100

    Added conllu evaluation and loading utilities

--- scripts/trankit/evaluate_trankit.sh
+++ scripts/trankit/evaluate_trankit.sh
@@ -1,6 +1,8 @@
 
 source environments/trankit/bin/activate
 
+VERSION="1.1.1"
+
 # run evaluation on two variants of the model
 # using two variants of foundational model for embeddings
 for MODEL in "ancient-greek" "ancient-greek-perseus" 
@@ -8,5 +10,10 @@ do
     for EMBEDDING in "xlm-roberta-base" "xlm-roberta-large"
     do
         python3 scripts/trankit/predict_trankit.py --model "$MODEL" --embedding "$EMBEDDING"
+        for CORPUS in "joint" "perseus" "proiel"
+        do
+            python3 -m spacy convert predictions/trankit/${CORPUS}_${MODEL}_${EMBEDDING}.conllu predictions/trankit/ --converter conllu -n 10 
+            python3 scripts/evaluate_docbin.py predictions/trankit/${CORPUS}_${MODEL}_${EMBEDDING}.spacy corpus/binary/${CORPUS}.spacy metrics/trankit_${MODEL}_${EMBEDDING}/${VERSION}/${CORPUS}.json
+        done
     done
 done
--- scripts/trankit/predict_trankit.py
+++ scripts/trankit/predict_trankit.py
@@ -1,4 +1,4 @@
-"""
+"""Creates predictions with trankit.
 """
 import os
 import argparse
@@ -14,9 +14,8 @@ if not os.path.exists(OUT_DIR):
     os.makedirs(OUT_DIR)
 
 
-def main(pipeline, embedding) -> None:
-
-    nlp = Pipeline(pipeline, embedding=embedding)
+def main(pipeline_name: str, embedding_name: str) -> None:
+    nlp = Pipeline(pipeline_name, embedding=embedding_name)
 
     for dataset in ["proiel", "perseus", "joint"]:
         print(f" - Predicting: {dataset}")
@@ -24,25 +23,28 @@ def main(pipeline, embedding) -> None:
         with open(path) as in_file:
             text = in_file.read()
         doc = nlp(text)
-        predictions = [sentence['tokens'] for sentence in doc['sentences']]
-        predictions_flat = [token for sentence in predictions for token in sentence]
+        predictions = [sentence["tokens"] for sentence in doc["sentences"]]
+        predictions_flat = [
+            token for sentence in predictions for token in sentence
+        ]
         print(" - Saving")
-        save_conllu(predictions_flat, path=os.path.join(OUT_DIR, f"{dataset}_{pipeline}_{embedding}.conllu"))
+        save_conllu(
+            predictions_flat,
+            path=os.path.join(
+                OUT_DIR, f"{dataset}_{pipeline_name}_{embedding_name}.conllu"
+            ),
+        )
 
     print("DONE")
 
 
 if __name__ == "__main__":
-
     ap = argparse.ArgumentParser()
-    ap.add_argument('--model')
-    ap.add_argument('--embedding')
+    ap.add_argument("--model")
+    ap.add_argument("--embedding")
     args = ap.parse_args()
 
-    main(
-        pipeline=args.model,
-        embedding=args.embedding
-    )
+    main(pipeline_name=args.model, embedding_name=args.embedding)
 
 
 # #  --- test ---
